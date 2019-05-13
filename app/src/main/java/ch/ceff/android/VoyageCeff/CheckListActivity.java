7231d4a9d9b0263796c7987419397a69a03e5bd7
Christian <christian.theron@ceff.ch>
2019-05-13 15:23:27 +0200

    Divers améliorations et avancements
    TODO : Amélioré l'ajout des checkboxs

--- app/src/main/java/ch/ceff/android/VoyageCeff/CheckListActivity.java
+++ app/src/main/java/ch/ceff/android/VoyageCeff/CheckListActivity.java
@@ -7,9 +7,12 @@ import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 public class CheckListActivity extends AppCompatActivity {
 
@@ -39,8 +42,28 @@ public class CheckListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "Hello world");
+                Log.d(TAG, "I'm trying to add something");
+                newBox("Test");
             }
         });
     }
+
+    private void newBox(String s) {
+        //TODO: Implement everything to use recycler view !
+        RecyclerView rec = findViewById(R.id.check_list_recycler_view);
+        CheckBox check = new CheckBox(this);
+        check.setText(s);
+        rec.addView(check);
+        Log.d(TAG, "It's suppose to add now");
+
+        /*Old code with linear :
+        LinearLayout lay = findViewById(R.id.check_list_linear_layout);
+        CheckBox check = new CheckBox(this);
+        check.setText(s);
+        lay.addView(check);
+        Log.d(TAG, "It's suppose to add now");
+         * This has placements issues
+         */
+    }
+
 }
