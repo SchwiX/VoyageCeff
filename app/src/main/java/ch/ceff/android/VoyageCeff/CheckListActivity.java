11195dd620291afe703c6059066ad541e976cb4d
Christian <christian.theron@ceff.ch>
2019-05-13 13:37:24 +0200

    Modifications divers
    Ajout de la Fab et divers Checkbox de test

--- app/src/main/java/ch/ceff/android/VoyageCeff/CheckListActivity.java
+++ app/src/main/java/ch/ceff/android/VoyageCeff/CheckListActivity.java
@@ -1,7 +1,11 @@
 package ch.ceff.android.VoyageCeff;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckListActivity extends AppCompatActivity {
 
@@ -11,5 +15,13 @@ public class CheckListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_list);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Hello world");
+            }
+        });
     }
 }
