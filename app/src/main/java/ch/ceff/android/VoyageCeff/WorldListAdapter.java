00d41e037938fef3d358597f9b35af854be12fde
Christian <christian.theron@ceff.ch>
2019-05-14 09:19:39 +0200

    Ajout des checkboxes avec le bouton Fab
    TODO : Pouvoir choisir le text avant l'ajout

---
+++ app/src/main/java/ch/ceff/android/VoyageCeff/WorldListAdapter.java
@@ -0,0 +1,61 @@
+package ch.ceff.android.VoyageCeff;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder>{
+
+    private final ArrayList<String> mWordList;
+    private LayoutInflater mInflater;
+
+    class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        final CheckBox wordItemView; //représente le composant qui affichera le mot dans le layout
+        final WordListAdapter mAdapter; //représente l'adapter
+
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+        }
+
+        @Override
+        public void onClick(View v) {
+            int mPosition = getLayoutPosition();
+            String element = mWordList.get(mPosition);
+            mWordList.set(mPosition, "Cliqué! " + element);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public WordListAdapter(Context context, ArrayList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
+
+    @NonNull
+    @Override
+    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View mItemView = mInflater.inflate(R.layout.checklist_item, viewGroup, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder wordViewHolder, int i) {
+        String mCurrent = mWordList.get(i);
+        wordViewHolder.wordItemView.setText(mCurrent);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+}
+
