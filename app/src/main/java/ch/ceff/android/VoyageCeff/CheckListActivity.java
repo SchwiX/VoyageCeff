e3b4825e41d5af73383cbd6c2dd2ba42098552f3
Christian <christian.theron@ceff.ch>
2019-05-15 09:56:34 +0200

    Delete and checked style
    Ajout du boutton delete avec la fonction derrière pour la suppression
    Modification de texte si la checkbox est checked

--- app/src/main/java/ch/ceff/android/VoyageCeff/CheckListActivity.java
+++ app/src/main/java/ch/ceff/android/VoyageCeff/CheckListActivity.java
@@ -13,7 +13,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
@@ -78,7 +80,6 @@ public class CheckListActivity extends AppCompatActivity {
 
         builder.setView(input);
 
-
         // Set up the buttons
         builder.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
             @Override
@@ -102,6 +103,4 @@ public class CheckListActivity extends AppCompatActivity {
         mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
         mRecyclerView.smoothScrollToPosition(wordListSize);
     }
-
-
 }
