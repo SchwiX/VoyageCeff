d48a8a7a573cb47c74a639fbfa59f8e9d335f4b2
xlucxs <lucas.perrin@ceff.ch>
2019-05-29 11:28:13 +0200

    Fin jour

---
+++ app/src/main/java/ch/ceff/android/VoyageCeff/AppDatabase.java
@@ -0,0 +1,30 @@
+package ch.ceff.android.VoyageCeff;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {LocalDateParceable.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+
+    public abstract LocalDateParceable userDao();
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "local-database")
+                            // allow queries on the main thread.
+                            // Don't do this on a real app! See PersistenceBasicSample for an example.
+                            .allowMainThreadQueries()
+                            .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+}
