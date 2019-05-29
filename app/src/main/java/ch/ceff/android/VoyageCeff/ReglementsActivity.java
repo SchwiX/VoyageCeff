b9afd82a142879f3c149d432c64789389f8493a1
Cyril <cyril.despontin@ceff>
2019-05-29 11:27:23 +0200

    fix x 1000

--- app/src/main/java/ch/ceff/android/VoyageCeff/ReglementsActivity.java
+++ app/src/main/java/ch/ceff/android/VoyageCeff/ReglementsActivity.java
@@ -1,27 +1,22 @@
 package ch.ceff.android.VoyageCeff;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
 
 public class ReglementsActivity extends AppCompatActivity {
 
@@ -34,6 +29,22 @@ public class ReglementsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reglements);
+
+        /** PART 1 **/
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        //int texteCouleur = preferences.getInt("getTexteCouleur", R.style.AppTheme);
+        int texteCouleur = preferences.getInt("getTexteCouleur", 0);
+        setTheme(texteCouleur);
+        /**  **/
+
+        setContentView(R.layout.activity_reglements);
+
+        /** PART 2 **/
+        String couleur = preferences.getString("getCouleur", "#008577");
+        int background = preferences.getInt("getBackground",R.color.blanc);
+        this.getWindow().getDecorView().setBackgroundResource(background);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor(couleur)));
+        /** **/
 
         FloatingActionButton fab = findViewById(R.id.id_add_reglements);
         fab.setOnClickListener(new View.OnClickListener() {
